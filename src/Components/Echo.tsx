2ac228f1cc6fddb483e2cd3230d665a29c5eec0f
royyanwibisono <royyanwibisono@gmail.com>
2023-03-17 19:00:01 +0700

    rapih rapih

--- src/Components/Echo.tsx
+++ src/Components/Echo.tsx
@@ -5,9 +5,8 @@ import { Configuration, ActiveLayer} from 'ion-sdk-js/lib/client';
 import { myContext } from '../lib/Context';
 import { User } from '../Entity/User/User_model';
 import { ContactDict } from '../Entity/User/Contact_model';
-import { SortNames } from '../lib/Utils';
 import { useNavigate } from 'react-router-dom';
-import { MdClose, MdMenu, MdMessage } from 'react-icons/md';
+import { MdClose, MdMenu, MdMessage, MdOutlineFullscreen } from 'react-icons/md';
 import { Transition } from '@headlessui/react';
 import Loading from './Loading';
 
@@ -67,34 +66,19 @@ const Echo: React.FC<EchoProps> = (props) =>{
   const hasMountedRef = React.useRef(false);
   React.useEffect(():void => {
     if (hasMountedRef.current) return
-    hasMountedRef.current = true;
+    hasMountedRef.current = true
 
-    if (ctx.Comm !== null && owner && ctx.VicallCli === null && props.target[owner] && props.target[owner].datas.room.id) join()
+    if (ctx.Comm && owner && !ctx.VicallCli && props.target[owner]?.datas?.room?.id) {
+      join()
+    }
 
   },[]);
-
-  // const enableAudio = () =>{
-  //   if (remoteVideoIsMuted) {
-  //     // Unmute all the current videoElements.
-  //     for (const streamInfo of Object.values(streams)) {
-  //       let { videoElement } = streamInfo;
-  //       videoElement.pause();
-  //       videoElement.muted = false;
-  //       videoElement.play();
-  //     }
-  //     // Set remoteVideoIsMuted to false so that all future autoplays
-  //     // work.
-  //     setRemoteVideoIsMuted(false)
-  
-  //     const button = document.getElementById("enable-audio-button");
-  //     button?.remove();
-  //   }
-  // }
 
   const createVideoElement = (stream: RemoteStream) => {
     // Create a video element for rendering the stream
     const remoteVideo = document.createElement("video");
     remoteVideo.srcObject = stream;
+    // remoteVideo.controls = true;
     remoteVideo.autoplay = true;
     remoteVideo.className = "rounded-lg";
     
@@ -110,33 +94,31 @@ const Echo: React.FC<EchoProps> = (props) =>{
     bg.className = "rounded-lg";
 
     const controlsElement = document.createElement("div");
-    controlsElement.style.display = 'block';
-    controlsElement.style.position = 'absolute';
-    controlsElement.style.bottom = '5px';
-    controlsElement.style.right = '5px';
+    controlsElement.className = "block absolute bottom-0 right-0 p1"
 
     const muteButton = document.createElement("button");
-    muteButton.textContent = "Mute"
+    muteButton.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3A4.5 4.5 0 0014 7.97v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path></svg>'
     muteButton.onclick = (event) => {
       event.stopPropagation();
       remoteVideo.muted = !remoteVideo.muted;
-      muteButton.textContent = remoteVideo.muted ? "Unmute" : "Mute";
+      muteButton.innerHTML = !remoteVideo.muted ? '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3A4.5 4.5 0 0014 7.97v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path></svg>' : 
+      '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M16.5 12A4.5 4.5 0 0014 7.97v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51A8.796 8.796 0 0021 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06a8.99 8.99 0 003.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"></path></svg>';
     };
-    muteButton.className = "rounded-full bg-gray-700 text-white px-2 py-1 mr-2";
+    muteButton.className = "bg-opacity-10 hover:bg-opacity-40 bg-blue-500 font-bold py-2 px-2 rounded";
     controlsElement.appendChild(muteButton);
     
 
     const maximizeButton = document.createElement("button");
-    maximizeButton.textContent = "Maximize";
+    maximizeButton.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"></path></svg>';
     maximizeButton.onclick = (event) => {
       event.stopPropagation();
       remoteVideo.requestFullscreen();
     };
-    maximizeButton.className = "rounded-full bg-gray-700 text-white px-2 py-1 mr-2";
+    maximizeButton.className = "bg-opacity-10 hover:bg-opacity-40 bg-blue-500 font-bold py-2 px-2 rounded";
     controlsElement.appendChild(maximizeButton);
 
     const container = document.createElement("div");
-    container.className = "relative flex flex-col gap-2 flex-1 min-w-[260px] max-w-md justify-between border border-eprimary-color rounded-lg border-dashed p-[1px]";
+    container.className = "relative flex flex-col gap-2 flex-1 min-w-[260px] max-w-md justify-between border rounded-lg border-dashed p-[1px]";
     container.appendChild(bg);
     container.appendChild(remoteVideo);
     container.appendChild(controlsElement)
@@ -224,6 +206,15 @@ const Echo: React.FC<EchoProps> = (props) =>{
       cl.onspeaker = (ev: string[]) => {
         if (ev.length > 0){
           console.log("Echo onspeaker " + ev)
+          // for (let index = 0; index < ev.length; index++) {
+          //   if (streams[ev[index]]) {
+          //     const { videoElement } = streams[ev[index]];
+          //     if (videoElement) {
+          //       videoElement.parentElement.className = "relative flex flex-col gap-2 flex-1 min-w-[260px] max-w-md justify-between border border-eprimary-color rounded-lg border-dashed p-[1px]";
+          //       //add time out to delete videoElement.parentElement.className if this event not being called again after 5 second
+          //     }
+          //   }
+          // }
         }
       };
 
@@ -268,6 +259,14 @@ const Echo: React.FC<EchoProps> = (props) =>{
     ctx.SharedScreen = null
   }
 
+  const maxWebCam = () => {
+    elWebcam.current?.requestFullscreen()
+  }
+
+  const maxSharedScreen = () => {
+    elSharedScreen.current?.requestFullscreen()
+  }
+
   // if (localStream!== null) {
   //   localStream.getTracks()[0].removeEventListener("onended",unpublishScreenSharing)
   //   localStream.getTracks()[0].onended = unpublish
@@ -306,19 +305,32 @@ const Echo: React.FC<EchoProps> = (props) =>{
         audio: true,
         codec: "vp8",
       }).then((media):void => {
-        media.unmute("audio")
+        // media.unmute("audio")
         console.log("on set webcam stream")
         setMutedWebcamVideo(false)
         setMutedWebcamAudio(false)
         setLocalStream( media );
         elWebcam!.current!.srcObject = media;
         elWebcam!.current!.autoplay = true;
-        elWebcam!.current!.controls = true;
-        // localVideo!.current!.muted = false;
+        // elWebcam!.current!.controls = true;
+        elWebcam!.current!.muted = true;
         client?.publish(media);
         ctx.WebCam = media
       }).catch(console.error);
     }
+    // else if (localStream){
+    //   LocalStream.getDisplayMedia({
+    //     resolution: 'hd',
+    //     video: true,
+    //     codec: "vp8"
+    //   }).then((media) => {
+    //     // Replace old video track with new video track
+    //     const [videoTrack] = media.getVideoTracks();
+    //     const [oldVideoTrack] = localStream.getVideoTracks();
+    //     localStream.removeTrack(oldVideoTrack);
+    //     localStream.addTrack(videoTrack);
+    //   }).catch(console.error);
+    // }
     else{
       if (localStreamSs) {
         unpublishScreenSharing()
@@ -335,7 +347,7 @@ const Echo: React.FC<EchoProps> = (props) =>{
         setLocalStreamSs( media );
         elSharedScreen!.current!.srcObject = media;
         elSharedScreen!.current!.autoplay = true;
-        elSharedScreen!.current!.controls = true;
+        // elSharedScreen!.current!.controls = true;
         // localSharedScreen!.current!.muted = false;
         client?.publish(media);
         ctx.SharedScreen = media
@@ -414,11 +426,10 @@ const Echo: React.FC<EchoProps> = (props) =>{
         </div>
         
         <div ref={divRemote} className="flex items-center justify-center flex-wrap gap-2 w-full min-h-screen max-h-screen overflow-auto">
-          <div className={`${localStream? "" :"hidden"} flex flex-col flex-1 min-w-[260px] max-w-md`}>
-            <div className="relative flex flex-wrap gap-2 justify-end">
-              <div className="absolute top-0 flex flex-row gap-2 z-10">
-                <button onClick={unpublish} className="bg-opacity-0 hover:bg-opacity-20 bg-red-500 font-bold py-2 px-2 rounded"><MdClose size={20} /></button>
-              </div>
+          <div className={`${localStream? "" :"hidden"} flex flex-col flex-1 min-w-[260px] max-w-md relative`}>
+            <div className="absolute top-0 right-0 z-10">
+              <button onClick={maxWebCam} className="bg-opacity-10 hover:bg-opacity-40 bg-blue-500 font-bold py-2 px-2 rounded"><MdOutlineFullscreen size={20} /></button>
+              <button onClick={unpublish} className="bg-opacity-10 hover:bg-opacity-40 bg-red-500 font-bold py-2 px-2 rounded"><MdClose size={20} /></button>
             </div>
             <video
               ref={elWebcam}
@@ -429,13 +440,11 @@ const Echo: React.FC<EchoProps> = (props) =>{
               controls={false}
               className="rounded-lg"
               defaultChecked />
-            
           </div>
-          <div className={`${localStreamSs? "visible" :"hidden invisible"} flex flex-col flex-1 min-w-[260px] max-w-md justify-between`}>
-            <div className="relative flex flex-wrap gap-2 justify-end">
-              <div className="absolute top-0 flex flex-row gap-2 z-10">
-                <button onClick={unpublishScreenSharing} className="bg-opacity-0 hover:bg-opacity-20 bg-red-500 font-bold py-2 px-2 rounded"><MdClose size={20} /></button>
-              </div>
+          <div className={`${localStreamSs? "" :"hidden "} flex flex-col flex-1 min-w-[260px] max-w-md relative`}>
+            <div className="absolute top-0 right-0 z-10">
+              <button onClick={maxSharedScreen} className="bg-opacity-10 hover:bg-opacity-40 bg-blue-500 font-bold py-2 px-2 rounded"><MdOutlineFullscreen size={20} /></button>
+              <button onClick={unpublishScreenSharing} className="bg-opacity-10 hover:bg-opacity-40 bg-red-500 font-bold py-2 px-2 rounded"><MdClose size={20} /></button>
             </div>
             <video
               ref={elSharedScreen}
--- src/global.tsx
+++ src/global.tsx
@@ -1,5 +1,5 @@
-export const API_URL: string = 'http://localhost:7000';
-export const API_WSURL: string = 'ws://localhost:7000';
+export const API_URL: string = 'http://192.168.100.7:7000';
+export const API_WSURL: string = 'ws://192.168.100.7:7000';
 export const SITE_NAME: string = 'PESATU';
 export const UserCountResult: number = 10;
 export const MessageLimit: number = 40;
