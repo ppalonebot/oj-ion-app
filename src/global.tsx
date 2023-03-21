49c79aadbd6ef13efeb535aa602550ccda7ac082
royyanwibisono <royyanwibisono@gmail.com>
2023-03-21 22:33:20 +0700

    harcoded the port

--- src/global.tsx
+++ src/global.tsx
@@ -9,7 +9,7 @@ if (hostname.includes('localhost') || hostname.includes('192.168.')) {
 }
 
 export const API_URL: string = 'http'+secure+'://' + host + port + '/api';
-export const API_WSURL: string = 'ws'+secure+'://'+ host + port + '/api';
+export const API_WSURL: string = 'ws'+secure+'://'+ host + (port===''? ':8080' : port) + '/api'; //harcoded the server port becouse blocked by proxy
 export const SITE_NAME: string = 'PESATU';
 export const UserCountResult: number = 10;
 export const MessageLimit: number = 40;
